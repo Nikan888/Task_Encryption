7e1b9cf2217999027f99a1336dbd66325ac94935
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-07-19 12:08:37 +0300

    Task_Encryption

--- Task_Encryption/Encryption.cs
+++ Task_Encryption/Encryption.cs
@@ -10,204 +10,57 @@ namespace Task_Encryption
 {
     public class Encryption
     {
-        public static void GenerateKeys(string publicKeyFileName, string privateKeyFileName)
+        private static string _privateKey;
+        private static string _publicKey;
+        private static UnicodeEncoding _encoder = new UnicodeEncoding();
+
+        public static void ShowRSA(string text)
         {
-            // Variables
-            CspParameters cspParams = null;
-            RSACryptoServiceProvider rsaProvider = null;
-
-            StreamWriter publicKeyFile = null;
-            StreamWriter privateKeyFile = null;
-
-            string publicKey = "";
-            string privateKey = "";
+            var rsa = new RSACryptoServiceProvider();
+            _privateKey = rsa.ToXmlString(true);
+            _publicKey = rsa.ToXmlString(false);
             
-            try
-            {
-                // Create a new key pair on target CSP
-                cspParams = new CspParameters();
-                cspParams.ProviderType = 1; // PROV_RSA_FULL
-                //cspParams.ProviderName; // CSP name
-                cspParams.Flags = CspProviderFlags.UseArchivableKey;
-                cspParams.KeyNumber = (int)KeyNumber.Exchange;
-                rsaProvider = new RSACryptoServiceProvider(cspParams);
+            Console.WriteLine("RSA // Text to encrypt: " + text);
+            var enc = Encrypt(text);
+            Console.WriteLine("RSA // Encrypted Text: " + enc);
+            var dec = Decrypt(enc);
+            Console.WriteLine("RSA // Decrypted Text: " + dec);
+        }
 
-                // Export public key
-                publicKey = rsaProvider.ToXmlString(false);
-                
-                // Write public key to file
-                publicKeyFile = File.CreateText(publicKeyFileName);
-                publicKeyFile.Write(publicKey);
-
-                // Export private/public key pair
-                privateKey = rsaProvider.ToXmlString(true);
-
-                // Write private/public key pair to file
-                privateKeyFile = File.CreateText(privateKeyFileName);
-                privateKeyFile.Write(privateKey);
-            }
-            catch (Exception ex)
-            {
-                // Any errors? Show them
-                Console.WriteLine("Exception generating a new key pair! More info:");
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                // Do some clean up if needed
-                if (publicKeyFile != null)
-                {
-                    publicKeyFile.Close();
-                }
-                if (privateKeyFile != null)
-                {
-                    privateKeyFile.Close();
-                }
-            }
-        } // Keys
-
-        public static void Encrypt(string publicKeyFileName, string plainFileName, string encryptedFileName)
+        public static string Decrypt(string data)
         {
-            // Variables
-            CspParameters cspParams = null;
-            RSACryptoServiceProvider rsaProvider = null;
-
-            StreamReader publicKeyFile = null;
-            StreamReader plainFile = null;
-
-            FileStream encryptedFile = null;
-
-            string publicKeyText = "";
-            string plainText = "";
-
-            byte[] plainBytes = null;
-            byte[] encryptedBytes = null;
-
-            try
+            var rsa = new RSACryptoServiceProvider();
+            var dataArray = data.Split(new char[] { ',' });
+            byte[] dataByte = new byte[dataArray.Length];
+            for (int i = 0; i < dataArray.Length; i++)
             {
-                // Select target CSP
-                cspParams = new CspParameters();
-                cspParams.ProviderType = 1; // PROV_RSA_FULL
-
-                //cspParams.ProviderName; // CSP name
-                rsaProvider = new RSACryptoServiceProvider(cspParams);
-
-                // Read public key from file
-                publicKeyFile = File.OpenText(publicKeyFileName);
-                publicKeyText = publicKeyFile.ReadToEnd();
-
-                // Import public key
-                rsaProvider.FromXmlString(publicKeyText);
-
-                // Read plain text from file
-                plainFile = File.OpenText(plainFileName);
-                plainText = plainFile.ReadToEnd();
-
-                // Encrypt plain text
-                plainBytes = Encoding.Unicode.GetBytes(plainText);
-                encryptedBytes = rsaProvider.Encrypt(plainBytes, false);
-
-                // Write encrypted text to file
-                encryptedFile = File.Create(encryptedFileName);
-                encryptedFile.Write(encryptedBytes, 0, encryptedBytes.Length);
+                dataByte[i] = Convert.ToByte(dataArray[i]);
             }
 
-            catch (Exception ex)
-            {
-                // Any errors? Show them
-                Console.WriteLine("Exception encrypting file! More info:");
-                Console.WriteLine(ex.Message);
-            }
+            rsa.FromXmlString(_privateKey);
+            var decryptedByte = rsa.Decrypt(dataByte, false);
+            return _encoder.GetString(decryptedByte);
+        }
 
-            finally
-            {
-                // Do some clean up if needed
-                if (publicKeyFile != null)
-                {
-                    publicKeyFile.Close();
-                }
-
-                if (plainFile != null)
-                {
-                    plainFile.Close();
-                }
-
-                if (encryptedFile != null)
-                {
-                    encryptedFile.Close();
-                }
-            }
-        } // Encrypt
-
-        public static void Decrypt(string privateKeyFileName, string encryptedFileName, string plainFileName)
+        public static string Encrypt(string data)
         {
-            // Variables
-            CspParameters cspParams = null;
-            RSACryptoServiceProvider rsaProvider = null;
-            StreamReader privateKeyFile = null;
-            FileStream encryptedFile = null;
-            StreamWriter plainFile = null;
-
-            string privateKeyText = "";
-            string plainText = "";
-
-            byte[] encryptedBytes = null;
-            byte[] plainBytes = null;
-
-            try
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(_publicKey);
+            var dataToEncrypt = _encoder.GetBytes(data);
+            var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
+            var length = encryptedByteArray.Count();
+            var item = 0;
+            var sb = new StringBuilder();
+            foreach (var x in encryptedByteArray)
             {
-                // Select target CSP
-                cspParams = new CspParameters();
-                cspParams.ProviderType = 1; // PROV_RSA_FULL
-                //cspParams.ProviderName; // CSP name
-                rsaProvider = new RSACryptoServiceProvider(cspParams);
+                item++;
+                sb.Append(x);
 
-                // Read private/public key pair from file
-                privateKeyFile = File.OpenText(privateKeyFileName);
-                privateKeyText = privateKeyFile.ReadToEnd();
-
-                // Import private/public key pair
-                rsaProvider.FromXmlString(privateKeyText);
-
-                // Read encrypted text from file
-                encryptedFile = File.OpenRead(encryptedFileName);
-                encryptedBytes = new byte[encryptedFile.Length];
-                encryptedFile.Read(encryptedBytes, 0, (int)encryptedFile.Length);
-
-                // Decrypt text
-                plainBytes = rsaProvider.Decrypt(encryptedBytes, false);
-
-                // Write decrypted text to file
-                plainFile = File.CreateText(plainFileName);
-                plainText = Encoding.Unicode.GetString(plainBytes);
-                plainFile.Write(plainText);
+                if (item < length)
+                    sb.Append(",");
             }
 
-            catch (Exception ex)
-            {
-                // Any errors? Show them
-                Console.WriteLine("Exception decrypting file! More info:");
-                Console.WriteLine(ex.Message);
-            }
-
-            finally
-            {
-                // Do some clean up if needed
-                if (privateKeyFile != null)
-                {
-                    privateKeyFile.Close();
-                }
-
-                if (encryptedFile != null)
-                {
-                    encryptedFile.Close();
-                }
-
-                if (plainFile != null)
-                {
-                    plainFile.Close();
-                }
-            }
-        } // Decrypt
+            return sb.ToString();
+        }
     }
 }
